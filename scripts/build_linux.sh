3b8a46f410c4c05016bbad78e410f8d6cb726cb2
Matthew Ballance <matt.ballance@gmail.com>
2024-07-24 18:40:13 -0700

    Update build
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ scripts/build_linux.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -x
+
+yum update -y
+yum install -y glibc-static 
+yum install -y java-11-openjdk-devel uuid-devel libuuid-devel
+
+echo "BUILD_NUM=${BUILD_NUM}" >> python/zsp_parser/__build_num__.py
+${IVPM_PYTHON} -m pip install ivpm cython
+${IVPM_PYTHON} -m ivpm update -a
+
+PYTHON=./packages/python/bin/python
+${PYTHON} -m pip install twine auditwheel ninja wheel cython
+${PYTHON} setup.py bdist_wheel
+
+for whl in dist/*.whl; do
+    ${PYTHON} -m auditwheel repair $whl
+    rm $whl
+done
