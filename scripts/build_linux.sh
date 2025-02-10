7f9029e9c4df792a10491c142f6586f6e28b6b1b
Matthew Ballance <matt.ballance@gmail.com>
2025-02-10 05:04:29 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,8 +5,8 @@ yum install -y glibc-static
 yum install -y java-11-openjdk-devel uuid-devel libuuid-devel
 
 echo "BUILD_NUM=${BUILD_NUM}" >> python/zsp_parser/__build_num__.py
-${IVPM_PYTHON} -m pip install ivpm cython
-${IVPM_PYTHON} -m ivpm update -a
+${IVPM_PYTHON} -m pip install ivpm cython setuptools --pre
+${IVPM_PYTHON} -m ivpm update -a --py-prerls-packages
 
 PYTHON=./packages/python/bin/python
 ${PYTHON} -m pip install twine auditwheel ninja wheel cython
