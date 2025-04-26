eee18b5bb5d7450f823234fa1f2bcd4bc093cfaf
Matthew Ballance <matt.ballance@gmail.com>
2025-04-26 16:54:54 +0000

    A bit more digging
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,11 +5,14 @@ yum install -y glibc-static
 yum install -y java-11-openjdk-devel uuid-devel libuuid-devel
 
 echo "BUILD_NUM=${BUILD_NUM}" >> python/zsp_parser/__build_num__.py
-${IVPM_PYTHON} -m pip install ivpm cython setuptools --pre
+${IVPM_PYTHON} -m pip install ivpm cython setuptools --pre --upgrade
 ${IVPM_PYTHON} -m ivpm update -a --py-prerls-packages
 
 PYTHON=./packages/python/bin/python
 ${PYTHON} -m pip install twine auditwheel ninja wheel cython
+
+echo "IVPM version:
+${PYTHON} -m pip show ivpm
 
 # First, do all the required code generation. This ensures the
 # Python package can be imported during final package build
